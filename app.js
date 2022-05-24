389678a017fbf2bf43578b66a218da97b330ad3c
seleznovladislav <sandayz@mail.ru>
2022-05-24 17:14:17 +0300

    header, footer, form

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require('express')
+const path = require('path');
 const app = express()
 const PORT = process.env.PORT || 5500
 
+// middleware
+app.use(express.static(path.resolve(__dirname, 'client')))
 
+// Отправка статического документа на клиент
+app.get('/', (req,res)=>{
+	res.sendFile(path.resolve(__dirname, 'client', 'form.html'))
+})
 
 
 app.listen(PORT, ()=>{
